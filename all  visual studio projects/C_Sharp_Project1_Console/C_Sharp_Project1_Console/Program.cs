a3f7a864b1cb3ad970b9d64bbcdd26460a081490
AAYUSH-SAHAY-24326711019 <aayush.mcastudent24.1062@cimage.in>
2025-12-16 09:24:43 +0530

    added c# code for variable

--- all  visual studio projects/C_Sharp_Project1_Console/C_Sharp_Project1_Console/Program.cs
+++ all  visual studio projects/C_Sharp_Project1_Console/C_Sharp_Project1_Console/Program.cs
@@ -81,11 +81,105 @@ public class StudentDetails
         Console.WriteLine("Roll No  "+rollNo+"\nStudentName: "+name+"\nAge: "+age);
     }
 
-    public static void Main()
+    public static void Main2()
     {
         StudentDetails sd = new StudentDetails();
         sd.Student();
         StudentDetails Aayush = new StudentDetails("Aayush",102,21);
         Aayush.Student();
+    }
+}
+
+/*Date : 16/12/2025 (Notes) 
+ we have already seen the local variable program as above.
+
+Instance variable aka non static variable at the class level.
+and outside the function and outside the constructor.
+They serve to be the property.
+
+As we make the object , its variables are made the seperate copy.
+They dont get destroyed until the object is destroyed.
+They are used throughout the class body. inside class functions
+or inside the class constructors
+ 
+ 
+ 
+ 
+ */
+
+class Marks
+{
+    public const int Max = 200;
+    //a constant value made by const keyword.
+    //it is handled by the compile time.
+    // we cannot make it change.
+    //it is a type of read only variable.
+    //behaviour like the static variable.
+    //it is initialized one time.
+    //like static accessible as a single copy to all the variables.
+    //access directly by the class name.
+    //but we cannot modify the values.
+
+
+    public readonly int id;
+    //Read only variable is initialzed only 1 time
+    //it is handled at the run time.
+    //it can be initialized in the constructor only once.
+    //const :  compile,declaration, static, fast, not object specific
+    //readonly :  runtime, init in declaration or function , object specific and slower
+
+
+    int engMarks, mathsMarks, phyMarks;
+    //These are the class level variables.
+
+    static int numberOfStudents;
+    //This is the static variable. known as the instance variable
+    //This is created in the heap variable. This is in memory specific location.
+    //Static variable single copy , shared among all the objects of the same class.
+    //make the counter , call in the constructor.
+    //not have garbage value and start with 0 when integer.
+    //can changed at any time of the program , for all.
+    //
+
+    //make init function
+    void init(int eng, int maths,int phy)
+    {
+        numberOfStudents += 1;
+        //increase the variable
+        engMarks = eng; mathsMarks = maths; phyMarks = phy;
+
+        //eg. of local variable inside the init :
+        //int localVar = 60; will show error when accessed , so does not exist in current context.
+        //it is accessible till the lifetime of init method.
+        //When control enters block / block of the functions , the variable is made
+        //exit from the block , the variable gets destroyed.
+        //instance variable made seperately for all the instances of the class.
+        //all have the garbage value by default.
+    }
+
+   static int dispNumberStudents()
+    {
+        return numberOfStudents;
+    }
+
+
+
+    //display function
+    void display()
+    {
+        //Console.WriteLine("English :" + engMarks + "\tMaths :" + mathsMarks + "\tPhysics :" + phyMarks+
+        //    "\nNumber of Students :"+Marks.numberOfStudents);
+
+        Console.WriteLine("English :" + engMarks + "\tMaths :" + mathsMarks + "\tPhysics :" + phyMarks+
+            "\n\n Serial Number of Students :"+Marks.dispNumberStudents());
+    }
+    public static void Main()
+    {
+        Marks mk1 = new Marks();
+        mk1.init(77, 85, 92);
+        mk1.display();
+        Marks mk2 = new Marks();
+        mk2.init(50, 50, 50);
+        mk2.display();
     }
 }
--- all  visual studio projects/C_Sharp_Project1_Console/C_Sharp_Project1_Console/obj/Debug/net10.0/C_Sharp_Project1_Console.AssemblyInfo.cs
+++ all  visual studio projects/C_Sharp_Project1_Console/C_Sharp_Project1_Console/obj/Debug/net10.0/C_Sharp_Project1_Console.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("C_Sharp_Project1_Console")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28d8b905f64ed136281820c861b00480af8ace77")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a28cbc2279d1f8b13035c422166fb46e9a8c6452")]
 [assembly: System.Reflection.AssemblyProductAttribute("C_Sharp_Project1_Console")]
 [assembly: System.Reflection.AssemblyTitleAttribute("C_Sharp_Project1_Console")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
