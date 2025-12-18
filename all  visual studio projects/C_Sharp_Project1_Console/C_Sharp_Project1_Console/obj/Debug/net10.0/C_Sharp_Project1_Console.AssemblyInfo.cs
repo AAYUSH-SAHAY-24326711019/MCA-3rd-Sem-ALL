aaa6d21228ce071c31e68f4fbbe7f106cd73b9a8
AAYUSH-SAHAY-24326711019 <aayush.mcastudent24.1062@cimage.in>
2025-12-18 10:24:26 +0530

    daiy add

--- all  visual studio projects/C_Sharp_Project1_Console/C_Sharp_Project1_Console/obj/Debug/net10.0/C_Sharp_Project1_Console.AssemblyInfo.cs
+++ all  visual studio projects/C_Sharp_Project1_Console/C_Sharp_Project1_Console/obj/Debug/net10.0/C_Sharp_Project1_Console.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("C_Sharp_Project1_Console")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb24b4a594cb1df0f83be0ac05cbe30428852731")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9983de779f208d9fcf5ac01cdd7df4b7d7ef1ee")]
 [assembly: System.Reflection.AssemblyProductAttribute("C_Sharp_Project1_Console")]
 [assembly: System.Reflection.AssemblyTitleAttribute("C_Sharp_Project1_Console")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- all  visual studio projects/C_Sharp_Project1_Console/C_Sharp_Project1_Console/program2.cs
+++ all  visual studio projects/C_Sharp_Project1_Console/C_Sharp_Project1_Console/program2.cs
@@ -1,11 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Runtime.ConstrainedExecution;
 using System.Text;
 
 
 class Game
 {
-    public static void Main(string[] args)
+    public static void Main21(string[] args)
     {
         //example of the formatted output..
         //There is also a concatenated method.
@@ -42,3 +43,51 @@ namespaces to resolve the conflict.
 
 
  */
+
+
+/*Date : 18/12/2025 
+ Topic conditional statements
+
+Note that if statement will compare the single value and the
+range of the values. ladder if , all the conditions , only one will run.
+its alternative is the switch case.
+
+
+ 
+ */
+public class Grades
+{
+
+    public static void Main() {
+        Console.WriteLine("user enter your marks");
+        int marks = Convert.ToInt16(Console.ReadLine());
+
+        //code of the comparison and grades generation
+    }
+
+
+}
+
+/*More than one level in the if statement is the Nested if statement.
+ 
+program : sub1,2,3 find check of the value above 75 marks and then get 80% above.
+make by nested if.
+{0},{1} or {var1} , {var2}
+
+switch(Expressio/variable/value to test){
+    //take the input and print the name of the month,
+    //does not take the float value , string , int number
+    // all cases must have the end with a break;
+    //end has the default that does not have the break.
+    
+}
+capital to lower
+char lower = char.ToLower(input);100,+10 to 200<(mkprgm)
+do while runs 1 time only
+
+while loop non infinite times , for loop finfite times.
+for loop all are the optional fields
+
+
+ */
+
