3d3b5b278f4aec0a61777bbc9f8190622a58ec72
AAYUSH-SAHAY-24326711019 <aayush.mcastudent24.1062@cimage.in>
2025-12-27 17:10:39 +0530

    daily push

--- all  visual studio projects/C_Sharp_Project1_Console/C_Sharp_Project1_Console/properties_demo.cs
+++ all  visual studio projects/C_Sharp_Project1_Console/C_Sharp_Project1_Console/properties_demo.cs
@@ -52,3 +52,70 @@ namespace C_Sharp_Project1_Console
         }
     }
 }
+/*This is the read write property
+some property is read only : only the get method is there
+some property is write only : only the set methode is there,
+you can write set then get or get then set .
+but on execution is always set then get only.
+
+make class object -> Then obj.Property = value; will call the set 
+now do int value = obj.Property; will call the get
+
+Some of them are the static variables.
+Then for that field
+
+A class can have the static field with the static property
+
+
+eg
+private static int x;
+
+public static int X{
+    get{
+        return x;
+    }
+    set{
+        x=value;
+    }
+}
+
+Value and the reference types depend on the data.
+Direct deal with the value : call by the value
+Direct deal with the memory adderess : call by the address
+
+keep the value is by value
+keep the memory reference of the other variable : we will use the by referemce
+
+value types are implemented through reference type{arrays,collections}
+and enums and structs are the type of the value type.
+
+class is always the reference type
+local variable in the stack memory
+and the new object / reference type is stored in the heap memory
+
+structure is a value type data type
+structure is a user defined data type containing all the variables of similar type
+or dissimilar type with the security being : all public store in the stack.
+while the class has all of its things by default private. it is the reference type
+
+Structure has no inheritance , but the class has the inheritance.
+Structure is a single independent unit will not come under the oops.
+we can use a structure inside a structure but cannot be inherited.
+
+make the structure when many variables of same time.
+The structure does not have the constructor in it.
+
+
+
+structure make a single unit that will combine the variables under it.
+
+generic : run time the data type will change
+
+make the structure : then name it the bill details.
+we will use the new keyword to make the structure.
+Then the reference(object) will be used to set the values
+To clear the screen , we will use the Console.Clear()
+and use the console.ReadKey(); to hold the screen
+
+
+*/
